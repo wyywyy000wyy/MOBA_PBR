cdc241d725122e1016a0bdf0ae1fc1c4bcab619d
wangyiyi <yiyi.wang@igg.com>
2023-06-14 20:55:05 +0800

    sectr

---
+++ Assets/Procedural Worlds/SECTR/Scripts/Editor/SECTRStdMenu.cs
@@ -0,0 +1,46 @@
+﻿// Copyright © 2018 Procedural Worlds Pty Limited.  All Rights Reserved.
+using UnityEngine;
+using UnityEditor;
+using PWCommon1;
+
+namespace SECTR.Internal
+{
+    public class SECTRStdMenu : Editor
+    {
+        /// <summary>
+        /// Show tutorials
+        /// </summary>
+        [MenuItem("Window/" + PWConst.COMMON_MENU + "/SECTR/Show SECTR Tutorials...", false, 60)]
+        public static void ShowTutorial()
+        {
+            Application.OpenURL(PWApp.CONF.TutorialsLink);
+        }
+
+        /// <summary>
+        /// Show support page
+        /// </summary>
+        [MenuItem("Window/" + PWConst.COMMON_MENU + "/SECTR/Show SECTR Support, Lodge a Ticket...", false, 61)]
+        public static void ShowSupport()
+        {
+            Application.OpenURL(PWApp.CONF.SupportLink);
+        }
+
+        /// <summary>
+        /// Show review option
+        /// </summary>
+        [MenuItem("Window/" + PWConst.COMMON_MENU + "/SECTR/Please Review SECTR...", false, 62)]
+        public static void ShowProductAssetStore()
+        {
+            Application.OpenURL(PWApp.CONF.ASLink);
+        }
+
+        /// <summary>
+        /// Show the welcome screen for this app
+        /// </summary>
+        [MenuItem("Window/" + PWConst.COMMON_MENU + "/SECTR/Show SECTR Welcome...", false, 63)]
+        public static void ShowProductWelcome()
+        {
+            PWWelcome.ShowWelcome(PWApp.CONF);
+        }
+    }
+}
---
+++ Assets/Procedural Worlds/SECTR/Sectr_ReadMe.txt
@@ -0,0 +1,25 @@
+Welcome and thanks for buying Sectr!
+
+! Please don't delete or move this file as it is used to determine the Sectr root directory. !
+
+** Version history **
+
+Version 2019.0.2
+
+- Improved export so that sector child objects will be exported in the same order as they were in the original sector
+  Thanks to discord user @ropemonkey for the suggestion!
+- Improved speed for reverting sectors with a deeper child structure
+  Thanks to discord user @Pilgrim for the suggestion!
+
+Version 2019.0.1
+
+- Fixed an issue with Stream Export where exported objects in Sectors would lose their prefab state
+- Added a message box when GameObjects could not be sorted away by the Drag and Drop Box to inform the user
+- Changed the options to spawn GameObjects spawned by Gaia so that it will only process activated spawners
+- When deactivating the terrain split option in the terrain window, the window will now remember your settings when switching through different terrains.
+- Reviewed the documentations, added proper PDF bookmarks for better navigation
+- Fixed an issue that could lead to build errors in Unity 2017 and below.
+- Fixed 2 obsoletion warnings
+- Stabilized the package import process under Unity 2019.1 and above
+
+Version 2019.0.0, Initial version. 
